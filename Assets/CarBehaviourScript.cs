1b16b4db088c3723d9f8fa41d046e3794850397a
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-11-29 15:43:53 +0900

    Initial commit

---
+++ Assets/CarBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviourScript : MonoBehaviour {
+
+    private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+
+        pos.x = 5 * Mathf.Sin(timer);
+        pos.z = 20 + Mathf.Sin(timer * 0.2f);
+
+        transform.position = pos;
+
+        timer += Time.deltaTime;
+	}
+}
